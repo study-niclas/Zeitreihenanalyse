35267d8afbf0c879f6e9a8f3cda1f9d7e3caf846
study-niclas <niclas.gencer@student.fhws.de>
2025-05-07 13:26:56 +0200

    code(TODO): Established first steps to work with the Data and saved it into the main.py file

---
+++ src/main.py
@@ -0,0 +1,8 @@
+#here is the main code
+
+#TODO Daten einlesen mit yfinance
+#TODO Daten bereinigen nach aussreissern und evtl. etwas glätten
+#TODO Daten speichern (.csv im data ordner oder .db)
+#TODO Daten splitten(80/20) für Modelltest - wie gut ist der predict
+#TODO ACF and PACF https://medium.com/@kis.andras.nandor/understanding-autocorrelation-and-partial-autocorrelation-functions-acf-and-pacf-2998e7e1bcb5
+#TODO Daten Visualisieren und va predict zu Relität unterschiede highlighten
