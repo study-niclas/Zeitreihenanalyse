7afef4ccc22e993889d61ae16ba18456a6f2a965
study-niclas <niclas.gencer@student.fhws.de>
2025-05-12 15:04:47 +0200

    code(mbg): some changes

--- src/data_visualization_mbg.py
+++ src/data_visualization_mbg.py
@@ -5,11 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.pyplot as plt
 
-ticker = yf.Ticker("MBG.DE")
-df = ticker.history(period="10y")  # 10 Jahre Rückblick
 
-
-y = df['Close'].dropna()
 y.plot(title="Mercedes-Benz")
 plt.ylabel("EUR")
 plt.show()
